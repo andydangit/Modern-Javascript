7f7344a80bf954fa66679592da5b99e214481ffd
Andy Dang <andy.dang.it@gmail.com>
2020-01-07 12:47:03 -0600

    timeStamp: 16:21

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,2 +1,17 @@
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2020;
+
+
+console.log(age, year);
+
+
+age = 30;
+console.log(age);
+
+const points = 100; 
+console.log(points);
+
+
+// this is the old way to write variable
+var score = 75;
+console.log(score);
