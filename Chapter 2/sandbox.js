1f2b04e12692971a415a6935ad7cc971df150b78
Andy Dang <andy.dang.it@gmail.com>
2020-01-07 15:03:28 -0600

    String

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,19 +1,44 @@
-let age = 25;
-let year = 2020;
+// let age = 25;
+// let year = 2020;
+// console.log(age, year);
 
 
-console.log(age, year);
+// age = 30;
+// console.log(age);
+
+// const points = 100; 
+// console.log(points);
 
 
-age = 30;
-console.log(age);
-
-const points = 100; 
-console.log(points);
+// // this is the old way to write variable
+// var score = 75;
+// console.log(score);
 
 
-// this is the old way to write variable
-var score = 75;
-console.log(score);
 
-//testing 
+// strings
+console.log('hello, world');
+
+let email = "mario@thenetninja.co.uk";
+console.log(email);
+
+//string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
