dbfae8bf48d565e9a71ccafdb0c845efb9d7d33e
Andy Dang <andy.dang.it@gmail.com>
2020-01-07 20:15:51 -0600

    Numbers

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,16 +1,36 @@
-// common string methods 
+let radius = 10;
+const pi = 3.14;
 
-let email = 'mario@thenetninja.co.uk';
+// console.log(radius, pi);
 
-// let result = email.lastIndexOf('n');
+// math operators +, -, *, /, **, %
 
-// let result = email.slice(0,5);
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi * radius ** 2;
 
-// let result = email.substr(4,10);
+// order of operation -B I D M A S
 
-// let result = email.replace('m', 'w');
+// let result = 5 * (10-3) ** 2;
 
-let result = email.replace('n', 'w');
+// console.log(result);
+
+let likes = 10;
+
+// likes ++;
+// likes --;
+// likes += 10;
+// likes -= 5;
+// likes /=2;
+
+// console.log(likes);
+
+
+
+// NaN - not a number
+// console.log(5 / 'hello')
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes';
 
 console.log(result);
-
