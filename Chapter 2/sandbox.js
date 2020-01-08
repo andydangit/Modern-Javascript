e3ee16af0d277af5b37a821791d761b6a5b3882b
Andy Dang <andy.dang.it@gmail.com>
2020-01-07 20:31:34 -0600

    template strings

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,36 +1,21 @@
-let radius = 10;
-const pi = 3.14;
+// template strings 
+const title = "best reads of 2019";
+const author = "Mario";
+const likes = 30; 
 
-// console.log(radius, pi);
-
-// math operators +, -, *, /, **, %
-
-// console.log(10/2);
-// let result = radius % 3;
-// let result = pi * radius ** 2;
-
-// order of operation -B I D M A S
-
-// let result = 5 * (10-3) ** 2;
-
+// concatenation way 
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
 // console.log(result);
 
-let likes = 10;
-
-// likes ++;
-// likes --;
-// likes += 10;
-// likes -= 5;
-// likes /=2;
-
-// console.log(likes);
-
-
-
-// NaN - not a number
-// console.log(5 / 'hello')
-// console.log(5 * 'hello');
-
-let result = 'the blog has ' + likes + ' likes';
-
+// template string  way  
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+
+// creating html templates
+let html = `
+<h2> ${title} </h2>
+<p> By ${author} </p>
+<span> This blog has ${likes} likes <span>
+`;
+
+console.log(html);
