75994d17d5c1716035fbc611238fb30523b3d764
Andy Dang <andy.dang.it@gmail.com>
2020-01-07 21:10:48 -0600

    Null

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,23 +1,3 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-
-// console.log(ninjas.length);
-
-// array methods
-// let results = ninjas.join(', ');
-// let results = ninjas.indexOf('chun-li');
-// let results = ninjas.concat(['ken', 'crystal']);
-let results = ninjas.push('ken');
- results = ninjas.pop();
-
-console.log(results);
-
+let age = null;
+ 
+console.log(age, age + 3, `the age is ${age}`);
