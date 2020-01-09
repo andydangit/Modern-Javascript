f5bc2da451d223ab7f503759899eb222fca0b9f1
Andy Dang <andy.dang.it@gmail.com>
2020-01-08 23:15:25 -0600

    Logical Operators

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -1,11 +1,11 @@
-// else if statements
+// logical operators - OR || and AND &&
 
-const password = 'p@ss';
+const password = 'p@ss12';
 
-if(password.length >= 12){
+if(password.length >= 12 && password.includes('@') && password.includes('s')){
     console.log('that password is mighty strong');
-} else if(password.length >= 8) {
-    console.log('that password is long enough');
+} else if(password.length >= 8 || password.includes('@') && password.length >=5) {
+    console.log('that password is strong enough');
 } else {
-    console.log('it is not long enought');
+    console.log('it is not strong enought');
 }
