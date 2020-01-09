bb7813415735562c3a220c4a147781308e951bd5
Andy Dang <andy.dang.it@gmail.com>
2020-01-08 22:52:43 -0600

    Else If Statements

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -1,18 +1,11 @@
-// if statements
-// const age = 25; 
-
-// if(age > 20){
-//     console.log('you are over 20 years old');
-// } 
-
-// const ninjuas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjuas.length > 3){
-//     console.log("that's a lot of ninjas");
-// }
+// else if statements
 
 const password = 'p@ss';
 
-if(password.length >= 8) {
+if(password.length >= 12){
+    console.log('that password is mighty strong');
+} else if(password.length >= 8) {
     console.log('that password is long enough');
+} else {
+    console.log('it is not long enought');
 }
