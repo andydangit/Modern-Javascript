561655799626ae55500e039212da93ecdca12ff2
Andy Dang <andy.dang.it@gmail.com>
2020-01-08 22:08:54 -0600

    Do While Loop

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -1,17 +1,7 @@
-// while loops
+// do while loops
 
-const names = ['shaun', 'mario', 'luigi'];
-
-// let i = 0;
-// while(i < 5){
-//     console.log('in loop: ', i);
-//     i++;
-// }
-
-let i = 0;
-while(i < names.length) {
-    console.log(names[i]);
+let i = 3;
+do{
+    console.log('val of i is: ', i);
     i++;
-}
-
-
+} while (i < 5);
