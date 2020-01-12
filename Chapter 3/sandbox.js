4b9559bfffaa283083eca67d1e3513504dc1bd86
Andy Dang <andy.dang.it@gmail.com>
2020-01-11 21:15:04 -0600

    Switch Statements

--- Chapter 3/sandbox.js
+++ Chapter 3/sandbox.js
@@ -1,19 +1,23 @@
-// break and continue 
+// switch statements
+const grade = "A";
 
-const scores = [50, 25, 0, 30, 100, 20, 10 ];
-
-for (let i = 0; i < scores.length; i++){
-
-    if(scores[i] === 0) {
-        continue;
-    }
-
-    console.log('your score: ', scores [i]);
-
-if (scores[i] === 100) {
-    console.log('congrats, you got the top score!');
-    break;
-}
-
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got an B!');
+        break;
+    case 'C':
+        console.log('You got an C!');
+        break;
+    case 'D':
+        console.log('You got an D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    default: 
+        console.log('not a valid grade');
 }
 
