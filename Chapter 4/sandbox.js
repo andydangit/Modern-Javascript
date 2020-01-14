fe4aec5427cd2778ed2171af26fff0b7749b9681
Andy Dang <andy.dang.it@gmail.com>
2020-01-14 16:20:18 -0600

    Methods

--- Chapter 4/sandbox.js
+++ Chapter 4/sandbox.js
@@ -1,36 +1,14 @@
-// practise arrow functions 
-
-// Question 1.
-
-// const greet = function (){
-//     return 'hello, world';
-// };
-
-// Ansewr: 
-const greet = () => ' hello, world';
-
-const result = greet();
-console.log(result);
+const name = 'shaun';
 
 
+// functions
 
-// Question 2.  
-// const bill = function(products, tax) {
-//     let total = 0; 
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total; 
-// }
+const greet = () => 'hello';
 
-//Answer:
-const bill =(products, tax) => {
-    let total = 0; 
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-}
+let resultOne = greet();
+console.log(resultOne);
 
 
-console.log(bill([10, 15, 30], 0.2))
+//methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
