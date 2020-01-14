b22859520c03c244018b57479753738372d26a51
Andy Dang <andy.dang.it@gmail.com>
2020-01-14 16:02:02 -0600

    Arrow Function

--- Chapter 4/sandbox.js
+++ Chapter 4/sandbox.js
@@ -1,14 +1,13 @@
-// returning values 
-
-// const speak = function(name = 'luigi', time = ' night'){
-//     console.log(`good ${time} ${time}`);
+// regular function 
+// const calcArea = function (radius){
+//     return 3.14 * radius**2;
 // };
 
-const calcArea = function (radius) {
-     return 3.14 * radius **2;
-    
-};
+
+
+// arrow function 
+const calcArea = radius => 3.14 * radius**2; 
+
 
 const area = calcArea(5);
-console.log(area);
-
+console.log('area is:', area);
