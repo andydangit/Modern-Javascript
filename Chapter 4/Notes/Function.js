5f36fc41e55645d9265f4a52c8b6180464280488
Andy Dang <andy.dang.it@gmail.com>
2020-01-14 10:12:03 -0600

    Function Declation

---
+++ Chapter 4/Notes/Function.js
@@ -0,0 +1,16 @@
+// Function declataion
+// note end of function does not have semicolon 
+function greet(){
+    console.log('hello there mate');
+}
+
+// function expression
+// note end of function epxression should have semicolon ";"
+const speak  = function(){
+    console.log('good day');
+};
+
+
+greet();
+
+speak();
