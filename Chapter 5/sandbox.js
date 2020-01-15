11545115d826b04e3e7c4f5a4e91c18b235959b0
Andy Dang <andy.dang.it@gmail.com>
2020-01-15 00:15:09 -0600

    Object Literals pt 2

--- Chapter 5/sandbox.js
+++ Chapter 5/sandbox.js
@@ -5,19 +5,21 @@ let user = {
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    },
+    logBlogs(){
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    }
 };
 
-console.log(user);
-console.log(user.name);
-
-user.age = 35;
-console.log(user.age);
-
-const key = 'location';
-
-console.log(user[key]);
-user['name'] = 'chun-li';
-console.log(user.name);
-
-console.log(typeof user);
+user.logBlogs();
+console.log(this);
