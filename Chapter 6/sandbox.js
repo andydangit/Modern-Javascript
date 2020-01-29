5829bd311d202b9538e2bcd4c9bedb6dabab7de8
Andy Dang <andy.dang.it@gmail.com>
2020-01-29 01:08:02 -0600

    Code Challenge

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -4,3 +4,20 @@
 // content.classList.add('error');
 // content.classList.remove('error');
 // content.classList.add('success');
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+    if(p.innerText.includes('success')){
+        p.classList.add('success');
+    }
+});
+
+const title = document.querySelector('.title');
+
+title.classList.toggle('test'); // add
+title.classList.toggle('test'); // remove
+
