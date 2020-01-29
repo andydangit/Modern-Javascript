dab8e80c0991b0f113b34a6b74d9ba56c561dd58
Andy Dang <andy.dang.it@gmail.com>
2020-01-28 20:49:57 -0600

    Chapter 6

---
+++ Chapter 6/sandbox.js
@@ -0,0 +1,16 @@
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para);
+// });
+// console.log(paras[2]);
+
+const errors = document.querySelectorAll('.errors');
+
+console.log(errors);
+
+
