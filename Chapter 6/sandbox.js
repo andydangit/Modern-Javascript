505274b21cce22e4d6373bc81b1152dbbfc8e4e3
Andy Dang <andy.dang.it@gmail.com>
2020-01-28 21:18:27 -0600

    InnerHTHML

--- Chapter 6/sandbox.js
+++ Chapter 6/sandbox.js
@@ -1,15 +1,21 @@
-// get an elements by ID 
-    // const title = document.getElementById('page-title');
-    // console.log(title);
+const para = document.querySelector('p');
 
-// get elements by their class name
-    // const errors = document.getElementsByClassName('error');
-    // console.log(errors);
-    // console.log(errors[0]);
+// console.log(para.innerText);
+    // para.innerText = ' ninjas are awesome!';
 
+    const paras = document.querySelectorAll('p');
 
-// get elements by their tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
+    // paras.forEach(para => {
+    //     console.log(para.innerText);
+    //     para.innerText += ' new text';
+    // });
 
+const content = document.querySelector('.content');
+    // console.log(content.innerHTML);
+    // content.innerHTML = '<h2> THIS IS A NEW H2 </h2> ';
+
+    const people = ['mario', 'luigi', 'yoshi'];
+
+    people.forEach(person => {
+        content.innerHTML += `<p>${person} </p>`;
+    });
